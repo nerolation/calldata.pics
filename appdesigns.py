565053ecf6c9603733aece686d70a9762bd88142
Nerolation <toni_w@gmx.at>
2024-02-27 02:31:08 +0100

    update py file

---
+++ appdesigns.py
@@ -0,0 +1,26 @@
+tabs_styles = {
+    'height': '44px',
+    'alignItems': 'center',
+}
+tab_style = {
+    'border': '2px solid #d6d6d6',
+    'padding': '6px',
+    'marginLeft': '1vw',
+    'marginRight': '1vw',
+    'backgroundColor': 'rgb(132, 164, 186)',
+    'color': 'white',
+    'fontSize': '16px',
+    'fontFamily': 'Ubuntu Mono, monospace',
+}
+
+tab_selected_style = {
+    'border': '2px solid #d6d6d6',
+    'backgroundColor': 'rgb(21, 100, 165)',
+    'fontSize': '17px',
+    'marginLeft': '1vw',
+    'marginRight': '1vw',
+    'fontWeight': 'bold',
+    'color': 'white',
+    'fontFamily': 'Ubuntu Mono, monospace',
+    'padding': '6px',
+}
