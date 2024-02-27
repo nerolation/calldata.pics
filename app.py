48de85bc3239b934611a726df68c7de103946817
Nerolation <toni_w@gmx.at>
2024-02-27 14:24:14 +0100

    update py file

--- app.py
+++ app.py
@@ -639,5 +639,6 @@ def update_tab_content(selected_tab):
 
 
 if __name__ == '__main__':
+    server = app.server
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
